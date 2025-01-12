ae74442fe9d2f1f19ce28bf808b7ac9aaa4c4f3d
Nama Kamu <email@kamu.com>
2025-01-13 00:36:47 +0800

    add profile and delete tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Card } from "./components/card"
+import {Profile} from "./components/profile"
 
 function App() {
   return (
     <div>
       <header>
         <h1>Agenda Master</h1>
-        <img src="/vite.svg" alt="Logo" />
       </header>
 
-      <Card />
       <p className="read-the-docs">
         Application to manage Agenda
       </p>
+
+      <Profile />
     </div>
   )
 }
