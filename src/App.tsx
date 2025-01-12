f5fe9b4e953159de9d782f293518380592ff60c6
Nama Kamu <email@kamu.com>
2025-01-12 23:12:13 +0800

    delete css

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,11 @@ function App() {
     <div>
       <header>
         <h1>Agenda Master</h1>
-        <img src="/vite.svg" alt="Logo"/>
+        <img src="/vite.svg" alt="Logo" />
       </header>
 
-      <div className="card">
-        
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Application to manage Agenda
       </p>
     </div>
   )
