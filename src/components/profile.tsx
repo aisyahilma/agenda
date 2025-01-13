d259cd66b23d9471f2b5542687f8b3dc785947be
Nama Kamu <email@kamu.com>
2025-01-13 11:02:06 +0800

    feat: gallery of profiles

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,8 +1,11 @@
-export function Profile () {
-    return (
-      <img
-        src="https://i.imgur.com/MK3eW3As.jpg"
-        alt="Katherine Johnson"
-      />
-    );
-  }
+export function Profile() {
+  const imageUrl = "https://st4.depositphotos.com/34458648/40874/v/450/depositphotos_408742582-stock-illustration-logo-luxury-design-vector-icon.jpg";
+  const altText = "AM";
+  const imageWidth = 100;
+
+  return (
+    <div>
+      <img src={imageUrl} alt={altText} width={imageWidth} height={75} />
+    </div>
+  );
+}
