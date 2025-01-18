6ec2248c8f6e26b67402ed0d7cefa2cf23befa18
Nama Kamu <email@kamu.com>
2025-01-18 13:57:05 +0800

    idk bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { TaskList } from "./components/task-list";
+import { Task } from "/types/task";
 
-const taskItems = [
-  { id: 1, text: "Breakfast", isCompleted: true },
-  { id: 2, text: "Sleep", isCompleted: false },
-  { id: 3, text: "Wake Up", isCompleted: false },
+const taskItems: Task[] = [
+  { id: 1, text: "Dinner", isCompleted: true, date: new Date() },
+  { id: 2, text: "Sleep", isCompleted: false, date: new Date() },
+  { id: 3, text: "Wake UP", isCompleted: false, date: new Date() },
 ];
 
 export function App() {
   return (
-    <div>
-      <header>
-        <h1>Task Management</h1>
-      </header>
+    <div className="flex justify-center">
+      <div className="space-y-5">
+        <header>
+          <h1 className="text-3xl font-bold text-blue-700 underline">
+            Task Management
+          </h1>
+        </header>
 
-      <p className="read-the-docs">Application to manage tasks. </p>
+        <p className="read-the-docs">Application to manage tasks. </p>
 
-      <TaskList taskItems={taskItems} />
+        <TaskList taskItems={taskItems} />
+      </div>
     </div>
   );
 }
